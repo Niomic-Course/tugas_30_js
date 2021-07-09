3be49188990bee907ac9de7a2391d4012d9aeb31
Saddam Husein <saddambinhusein2@gmail.com>
2021-07-10 00:14:34 +0800

    Tugas 30 JS Pengumpulan tugas 30 JS ke Niomic

---
+++ tugas30.js
@@ -0,0 +1,6 @@
+function panggilRegex(value) {
+    let data = /[es]/g;
+    console.log(value.match(data));
+}
+
+panggilRegex("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
